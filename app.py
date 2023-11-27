8f0705df789909c081dc1a4aa510e346048955af
chrisShort32 <144724027+chrisShort32@users.noreply.github.com>
2023-11-27 11:53:16 -0600

    changed stuff, website works on localhost:8000

--- app.py
+++ app.py
@@ -6,29 +6,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/breakfast/<choice>')
-def breakfast(choice):
-    return render_template('breakfast.html', choice= choice)
+@app.route('/breakfast/<item>')
+def breakfast(item):
+    return render_template('breakfast.html', item=item)
 
-@app.route('/breakfast-choice1/<breakfast_choice>')
+@app.route('/breakfast/<breakfast_choice>/breakfast_choice1')
 def breakfast_choice1(breakfast_choice):
     return render_template('breakfast_choice1.html', breakfast_choice = breakfast_choice)
 
-@app.route('/breakfast-choice2/<breakfast_choice>')
+@app.route('/breakfast/<breakfast_choice>/breakfast_choice2')
 def breakfast_choice2(breakfast_choice):
     return render_template('breakfast_choice2.html', breakfast_choice = breakfast_choice)
 
-@app.route('/dinner/<choice>')
-def dinner(choice):
-    return render_template('dinner.html', choice=choice)
+@app.route('/dinner/<item>')
+def dinner(item):
+    return render_template('dinner.html', item=item)
 
 
-@app.route('/dinner-choice1/<dinner_choice>')
+@app.route('/dinner/<dinner_choice>/dinner_choice1')
 def dinner_choice1(dinner_choice):
     return render_template('dinner_choice1.html',dinner_choice = dinner_choice)
 
 
-@app.route('/dinner-choice2/<dinner_choice>')
+@app.route('/dinner/<dinner_choice>/dinner_choice2')
 def dinner_choice2(dinner_choice):
     return render_template('dinner_choice2.html',dinner_choice = dinner_choice)
 

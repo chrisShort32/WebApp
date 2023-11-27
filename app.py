27475ab56fcff39d9a76594cf3a83a4005f1ee7a
chrisShort32 <144724027+chrisShort32@users.noreply.github.com>
2023-11-27 10:48:02 -0600

    changed breakfast and dinner

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ def index():
 
 @app.route('/breakfast/<choice>')
 def breakfast(choice):
-    return render_template('breakfast.html', choice = choice)
+    return render_template('breakfast.html', choice= choice)
 
-@app.route('/breakfast/<breakfast_choice>')
+@app.route('/breakfast-choice1/<breakfast_choice>')
 def breakfast_choice1(breakfast_choice):
     return render_template('breakfast_choice1.html', breakfast_choice = breakfast_choice)
 
-@app.route('/breakfast/<breakfast_choice>')
+@app.route('/breakfast-choice2/<breakfast_choice>')
 def breakfast_choice2(breakfast_choice):
     return render_template('breakfast_choice2.html', breakfast_choice = breakfast_choice)
 
@@ -23,12 +23,12 @@ def dinner(choice):
     return render_template('dinner.html', choice=choice)
 
 
-@app.route('/dinner/<dinner_choice>')
+@app.route('/dinner-choice1/<dinner_choice>')
 def dinner_choice1(dinner_choice):
     return render_template('dinner_choice1.html',dinner_choice = dinner_choice)
 
 
-@app.route('/dinner/<dinner_choice>')
+@app.route('/dinner-choice2/<dinner_choice>')
 def dinner_choice2(dinner_choice):
     return render_template('dinner_choice2.html',dinner_choice = dinner_choice)
 
